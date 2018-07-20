3eb8cb9466cc8a611c4ea8e88c3bfa5ed6885337
Lorenzo <lorenzo@lema-bkr.com>
2018-07-20 11:00:20 +0200

    ajustements dev/test/prod dans config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
-    //URL: "http://51.68.71.91:8091/", //Dev
-    URL: "http://51.68.71.91:8092/",   //Prod
-    //URL: "http://localhost:8092/",   //Local
+    //URL: "http://51.68.71.91:8091/", //Test
+    //URL: "http://51.68.71.91:8092/",   //Prod
+    URL: "http://localhost:8092/",   //Local
 
     apiPath: 'api/',
     googleMapsAPI: 'AIzaSyAHvIyK6M1SW6R1rYkXnTWNiCbbqE9gBVI'
