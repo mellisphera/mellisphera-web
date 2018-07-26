fbf494bc4d1be232d27e042501dffbee4fe5c298
nizarkhsib <nizar.khssib@gmail.com>
2018-07-26 13:13:34 +0200

    correction capteur

--- src/app/accueil/rapport/rapport.service.ts
+++ src/app/accueil/rapport/rapport.service.ts
@@ -16,12 +16,24 @@ export class RapportService {
     constructor(private http:HttpClient) {}
     // -- RUCHER -- RUCHER ---- RUCHER ---- RUCHER ---- RUCHER ---- RUCHER --
 
-    getNluResult(texte){
+    getNluResult(texte, idApiary){
         let body = JSON.stringify(texte);
-        return this.http.post('http://51.38.49.225:5000/star', { texte : texte } , httpOptions);
+        return this.http.post('http://51.38.49.225:5000/star', { texte : texte, idApiary : idApiary} , httpOptions);
     }
+    // pour afficher tout les observations de nature
+    getObservationsNature(idApiary) : Observable<any[]>{
+        return this.http.get<any[]>(CONFIG.URL+'report/nature/'+idApiary);
+    }  
+    // pour afficher tout les ruchers de l'utilsateur connecté
+    getObservationsRuche(idApiary) : Observable<any[]>{
+        return this.http.get<any[]>(CONFIG.URL+'report/ruche/'+idApiary);
+    }  
+    // pour afficher tout les ruchers de l'utilsateur connecté
+    getActionsApicoles(idApiary) : Observable<any[]>{
+        return this.http.get<any[]>(CONFIG.URL+'report/apicole/'+idApiary);
+    }  
 
-
+    // error handling
     errorHandler(error: HttpErrorResponse){
         return Observable.throw(error.message || "server error")
     }
