8ca076e28c1ce91e9042bcb80f78b00ba2fcd03d
marchandau <aurelie.mrchd@gmail.com>
2018-08-24 14:54:42 +0200

    ajout+modifs observations rucher et ruche

---
+++ src/app/accueil/ruche-rucher/processedReport.ts
@@ -0,0 +1,12 @@
+export class ProcessReport {
+ 
+    id : string;
+    date : string;
+    type : string;
+    sentence : string;
+    idApiary : string;
+    idHive : string;
+    nluScore : string;
+      
+    }
+    
