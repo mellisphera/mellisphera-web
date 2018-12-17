eca109d0fdb92607e595131e456abe7680942a97
Mickael Pezzoni <pezzoni.mickael@gmail.com>
2018-12-17 16:21:53 +0100

    correction fleurs

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 export const CONFIG = {
     //URL: "http://51.68.71.91:8091/", //Test
-    //URL: "http://51.68.71.91:8092/",   //Prod
-    URL: "http://localhost:8091/",   //Local
-    API_PY : "http://localhost:5000",
+    URL: "http://51.68.71.91:8092/",   //Prod
+    //URL: "http://localhost:8091/",   //Local
+    API_PY : "http://51.68.71.91:5000",
     apiPath: 'api/',
     googleMapsAPI: 'AIzaSyAHvIyK6M1SW6R1rYkXnTWNiCbbqE9gBVI',
-    URL_FRONT :'http://localhost:4200/'
-    //URL_FRONT :'http://51.68.71.91/app/'
+    //URL_FRONT :'http://localhost:4200/'
+    URL_FRONT :'http://51.68.71.91/app/'
 
 
 };
