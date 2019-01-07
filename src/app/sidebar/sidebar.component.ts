4505d282915086ae4064635b1b50b015a78e825e
Mickael Pezzoni <pezzoni.mickael@gmail.com>
2019-01-07 16:28:13 +0100

    sidebar en cours

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -25,6 +25,7 @@ export const ROUTES: RouteInfo[] = [
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  styleUrls:['./sidebar.component.scss'],
 
 })
 export class SidebarComponent implements OnInit {
