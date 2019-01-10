d1096ae19d43d5506494f9c69f6703647c02653d
Mickael Pezzoni <pezzoni.mickael@gmail.com>
2019-01-10 13:46:14 +0100

    modif script createUser

--- createUser.py
+++ createUser.py
@@ -10,35 +10,43 @@ port='43975'
 
 mongo = MongoClient('mongodb://'+user+':'+password+'@'+host+':'+port+'/')
 
-db =  mongo['tdebug']
+db =  mongo['Production']
 
 collection = db['User']
 
 valider = False
 while valider!= True:
-	print("Ajout d'un utilisateur")
-	username = input("User >")
-	password = input("Password >")
-	phone = input("Tel >")
-	email = input("email >")
-	if email == "" :
-		 email = username+"@mellisphera.fr"
-	connexion = 0
+  print("Ajout d'un utilisateur")
+  username = input("User >")
+  password = input("Password >")
+  fullName = input("Name >")
+  position = input("Position >")
+  country = input("Country >")
+  city = input("City >")
+  phone = input("Tel >")
+  email = input("email >")
+  if email == "" :
+    email = username+"@mellisphera.fr"
+  connexion = 0
 
-	user  = {
-		"createdAt" : datetime.datetime.now(), 
+  user  = {
+		"createdAt" : datetime.datetime.now(),
 		"login" : {
 			"username" : username,
 			"password" : password
 		},
 		"phone" : phone,
 		"email" : email,
+    "fullName" : fullName,
+    "position" : position,
+    "country" : country,
+    "city":city,
 		"connexions" : connexion
 	}
-	print(user)
-	confirm = input("Confirmer(y/n)>")
-	if(confirm == "y" or confirm == "Y"):
-		valider = True
+  print(user)
+  confirm = input("Confirmer(y/n)>")
+  if(confirm == "y" or confirm == "Y"):
+    valider = True
 
 users = db.User
 
@@ -46,4 +54,4 @@ user_id = users.insert_one(user).inserted_id
 print(user_id)
 
 
- 
+
