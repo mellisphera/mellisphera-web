d9c5eda5d6e35e4f9c462ad2ee5877fda50b5a88
mickael <pezzoni.mickael@gmail.com>
2019-02-20 10:07:46 +0100

    correction mineur

--- src/app/_model/rucher-model.ts
+++ src/app/_model/rucher-model.ts
@@ -1,14 +1,12 @@
-import { HexBase64BinaryEncoding } from "crypto";
-
 export interface RucherModel {
-    id? : string;
+    id?: string;
     latitude: string;
     longitude: string;
-    name: string
-    description : string;
-    createdAt : Date;
-    photo : string;
-    username : string;
-    codePostal : string;
-    ville : string;
+    name: string;
+    description: string;
+    createdAt: Date;
+    photo: string;
+    username: string;
+    codePostal: string;
+    ville: string;
 }
