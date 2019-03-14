3ce39dfa63b2c8a3d4a1898d0bba5182ef7820de
mickael2407 <pezzoni.mickael@gmail.com>
2019-03-14 18:54:36 +0100

    ajout feedback test

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  private urlSlack: string;
+  public feedbackForm: FormGroup;
+  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder) {
+    this.urlSlack = 'https://hooks.slack.com/services/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'
   }
 
+  ngOnInit() {
+    this.feedbackForm = this.formBuilder.group({
+      'comment': [null, Validators.required]
+    });
+  }
+
+  send() {
+    const body = { text: this.feedbackForm.value};
+    console.log(body);
+    this.httpClient.post<any>(this.urlSlack, body, httpOptions).subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
