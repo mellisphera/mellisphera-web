9b3bbb248c4398b22263bad459f99370258e92fd
Mickael <pezzoni.mickael@gmail.com>
2019-03-28 11:45:23 +0100

    ajout dashboard module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,37 +34,14 @@ import { UserloggedService } from './userlogged.service';
 import { AuthService } from './auth/Service/auth.service';
 import { AuthGuardService } from './auth/auth-guard.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { RapportService } from './dashboard/rapport/rapport.service';
-import { FleursFloraisonService } from './dashboard/fleurs-floraison/service/fleurs.floraison.service';
-import { RucheDetailService } from './dashboard/apiary/ruche-rucher/ruche-detail/ruche.detail.service';
-
-import { DailyRecordService } from './dashboard/service/dailyRecordService';
-import { DragAndCheckModule, Offsets } from 'ng2-drag-and-check';
-import { MeteoService } from './dashboard/meteo/Service/MeteoService';
-import { RucheService } from './dashboard/service/ruche.service';
-
-import { NgxEchartsModule } from 'ngx-echarts';
-import { CalendrierService } from './dashboard/meteo/Service/calendrier.service';
-import { GraphMeteoService } from './dashboard/meteo/Service/graph-meteo.service';
-import { ObservationService } from './dashboard/apiary/ruche-rucher/ruche-detail/observation/service/observation.service';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from './dashboard/shared/shared.module';
-import { PipeCapteur, SearchCapteur } from './dashboard/capteur/capteur.pipe';
-import { SearchFleurPipe, searchFleurByType, searchFleurByDate } from './dashboard/fleurs-floraison/service/search-fleur.pipe';
-import { DemoComponent } from './demo/demo.component';
 import { AdminComponent } from './admin/admin.component';
 import { SignupService } from './admin/service/signup.service';
 import { AuthInterceptorService } from './auth/Service/auth-interceptor.service';
-import { ngfModule } from 'angular-file';
 import { NotifierModule } from 'angular-notifier';
 import { ConfirmationPopoverModule } from 'angular-confirmation-popover';
 import { Erreur404ComponentComponent } from './erreur404-component/erreur404-component.component';
-import { StackApiaryComponent } from './dashboard/apiary/stack-apiary/stack-apiary.component';
-import { ApiaryNotesComponent } from './dashboard/apiary/apiary-notes/apiary-notes.component';
-import { WizardComponent } from './dashboard/wizard/wizard.component';
-import { ArchwizardModule } from 'angular-archwizard';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FeedbackComponent } from './feedback/feedback.component';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
@@ -74,44 +51,24 @@ export function httpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader {
 
 @NgModule({
   declarations: [
-    NouveauCapteurComponent,
-    CapteurComponent,
-    AppComponent,
     LoginComponent,
-    RucheRucherComponent,
-    MeteoComponent,
-    RapportComponent,
-    FleursFloraisonComponent,
-    PipeCapteur,
-    SearchCapteur,
-    SearchFleurPipe,
-    searchFleurByType,
-    searchFleurByDate,
-    AdminComponent,
-    HomeComponent,
-    Erreur404ComponentComponent,
-    StackApiaryComponent,
-    ApiaryNotesComponent,
-    WizardComponent,
-    FeedbackComponent,
+    AppComponent,
+    Erreur404ComponentComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     FormsModule,
     HttpModule,
-    FooterModule,
-    SidebarModule,
     RouterModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    DragAndCheckModule,
+/*     DragAndCheckModule,
     NgxEchartsModule,
-    ngfModule,
-    SharedModule,
-    ArchwizardModule,
-    BrowserAnimationsModule,
+    ngfModule, */
+/*     ArchwizardModule,
+ */    BrowserAnimationsModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -134,29 +91,13 @@ export function httpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader {
     })
   ],
   providers: [
-    CapteurService,
     UserloggedService,
     AuthService,
     AuthGuardService,
     JwtHelperService,
-    RucherService,
-    RapportService,
-    FleursFloraisonService,
-    RucheDetailService,
-    MeteoService,
-    DailyRecordService,
-    RucheService,
-    CalendrierService,
-    GraphMeteoService,
-    ObservationService,
     SignupService,
-    GraphStackService,
-    ConnectionsMapService,
-    ConnectionService,
-    CalendrierFSTLervice,
-    GraphHoneyService,
     GraphFlowerService,
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
   ],
   exports:[
     
