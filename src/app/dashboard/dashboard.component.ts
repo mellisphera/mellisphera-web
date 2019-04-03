5f7a179768fbe7e1d64a1559772d72bdfdf8b290
Mickael <pezzoni.mickael@gmail.com>
2019-04-03 11:04:10 +0200

    loading apiary

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { UserloggedService } from '../userlogged.service';
+import { LoadingService } from './service/loading.service';
+import { ngxLoadingAnimationTypes } from 'ngx-loading';
+
+const PrimaryWhite = '#ffffff';
+const SecondaryGrey = '#ccc';
+const PrimaryRed = '#dd0031';
+const SecondaryBlue = '#006ddd';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +17,15 @@ import { UserloggedService } from '../userlogged.service';
 export class DashboardComponent implements OnInit {
 
   message: string;
-  constructor(public login: UserloggedService) {
+  public ngxLoadingAnimationTypes = ngxLoadingAnimationTypes;
+  public loading = true;
+  public primaryColour = PrimaryWhite;
+  public secondaryColour = SecondaryGrey;
+  public coloursEnabled = false;
+  public config = { animationType: ngxLoadingAnimationTypes.none, primaryColour: this.primaryColour, secondaryColour: this.secondaryColour}
+
+  constructor(public login: UserloggedService,
+    public loadingService: LoadingService) {
     this.message = '';
   }
 
