9b25cd593af225a066cd9905478eecdfb4c9f8b3
Mickael <pezzoni.mickael@gmail.com>
2019-04-04 12:45:07 +0200

    correction stack & modifier 'selectRange' avec forkJoin

--- src/app/dashboard/apiary/ruche-rucher/ruche-detail/service/Record/record.service.ts
+++ src/app/dashboard/apiary/ruche-rucher/ruche-detail/service/Record/record.service.ts
@@ -104,6 +104,7 @@ export class RecordService {
           return { name: rec.recordDate, value: [rec.recordDate, rec.temp_int], sensorRef: rec.sensorRef };
         });
       if (!hive) {
+        console.log(lastMerge);
         return {
           series: [
             {
