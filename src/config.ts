12c2ad914f4bd9d72c47c9ec26812f864bf6ef8b
Mickael <pezzoni.mickael@gmail.com>
2019-05-27 09:44:20 +0200

    echelle daily us

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const CONFIG = {
+    URL: 'http://localhost:8091/',   //Local
+    API_PY : 'http://localhost:5000',
+    googleMapsAPI: 'AIzaSyAHvIyK6M1SW6R1rYkXnTWNiCbbqE9gBVI',
+    URL_FRONT :'https://test.mellisphera.com/',
+    PUBLIC_TOKEN : '',
+}
