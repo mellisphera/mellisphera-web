a7be8c87eb7516ccfb05fa98b806f92b9e154b92
mickael2407 <pezzoni.mickael@gmail.com>
2019-06-24 10:26:42 +0200

    add melliCharts module

--- src/app/dashboard/graph-echarts/GlobalGraph.ts
+++ src/app/dashboard/graph-echarts/GlobalGraph.ts
@@ -4,7 +4,7 @@ import { stringify } from 'querystring';
 import { UserloggedService } from '../../userlogged.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class GraphGlobal {
     public weight: {
@@ -28,15 +28,15 @@ export class GraphGlobal {
         unitT: string,
     };
 
-    public titresFR : Array<any>;
-    public titresEN : Array<any>;
+    public titresFR: Array<any>;
+    public titresEN: Array<any>;
 
     constructor(private userConfig: UserParamsService, public userService: UserloggedService) {
         this.weight = {
             name: '',
             min: null,
             max: 0,
-            interval : 0,
+            interval: 0,
             unitW: 'Kg'
         };
         this.temp = {
@@ -49,99 +49,108 @@ export class GraphGlobal {
             name: '',
             min: null,
             max: 0,
-            unitT: '° C',
+            unitT: '%',
         }
         if (this.userConfig.getUserPref().unitSystem === 'IMPERIAL') { // US
-            // If he is French
-            if(this.userService.getJwtReponse().country === "FR"){
-                this.weight.name = 'Poids lbs';
-                this.humidity.name = 'HUmidité %';
-            // EN
-            }else{
-                this.weight.name = 'Weight lbs';
-                this.humidity.name = 'Humidity %'
-            }
-            this.humidity.min = 0;
-            this.humidity.max = 100;
-            this.weight.min = 40;
-            this.weight.max = null;
-            this.humidity.unitT = '%';
-            this.weight.unitW = 'lbs';
-            this.weight.interval = 5;
-            // If he is French
-            if(this.userService.getJwtReponse().country === "FR"){
-                this.temp.name = 'Température (°F)';
-            // EN
-            }else{
-                this.temp.name = 'Temperature (°F)';
-            }
-            this.temp.min = 0;
-            this.temp.unitT = '° F';
-            this.temp.max = null;
-
+            this.setImperial();
         } else { // FR
-            // If he is French
-            if(this.userService.getJwtReponse().country === "FR"){
-                this.weight.name = 'Poids Kg';
-                this.humidity.name = 'HUmidité %';
-
-            // EN
-            }else{
-                this.weight.name = 'Weight Kg';
-                this.humidity.name = 'Humidity %'
-
-            }
-            this.humidity.name = 'Humidity %'
-            this.humidity.min = 0;
-            this.humidity.max = 100;
-            this.weight.min = 0;
-            this.humidity.unitT = '%';
-            this.weight.unitW = 'Kg';
-            this.weight.interval = 10;
-            this.weight.max = null;
-            // If he is French
-            if(this.userService.getJwtReponse().country === "FR"){
-                this.temp.name = 'Température (°C)';
-            // EN
-            }else{
-                this.temp.name = 'Temperature (°C)';
-            }
-            this.temp.unitT = '° C';
-            this.temp.min = 0;
-            this.temp.max = null;
+            this.setMetric();
         }
 
         //Table of titles : 
         //FR
         this.titresFR = [
-            {'graph' : 'reserveMiel', 'titre' : 'Stock de miel'},
-            {'graph' : 'DailyWeightIncomes', 'titre' : 'Poids journaliers'},
-            {'graph' : 'BroodDynamics', 'titre' : 'Dynamique du couvain'},
-            {'graph' : 'InternalRelativeHumidity', 'titre' : 'Humidité interne relative (max)'},
-            {'graph' : 'InternalTemperature', 'titre' : 'Température interne'},
-            {'graph' : 'ExternalTemperature', 'titre' : 'Température externe'},
-            {'graph' : 'WeightTemperature', 'titre' : 'Poids & Température'},
-            {'graph' : 'Humidity', 'titre' : 'Humidité (%)'},
-            {'graph' : 'loss', 'titre' : 'perte'},
-            {'graph' : 'Weight', 'titre' : 'Poids'}
+            { 'graph': 'reserveMiel', 'titre': 'Stock de miel' },
+            { 'graph': 'DailyWeightIncomes', 'titre': 'Poids journaliers' },
+            { 'graph': 'BroodDynamics', 'titre': 'Dynamique du couvain' },
+            { 'graph': 'InternalRelativeHumidity', 'titre': 'Humidité interne relative (max)' },
+            { 'graph': 'InternalTemperature', 'titre': 'Température interne' },
+            { 'graph': 'ExternalTemperature', 'titre': 'Température externe' },
+            { 'graph': 'WeightTemperature', 'titre': 'Poids & Température' },
+            { 'graph': 'Humidity', 'titre': 'Humidité (%)' },
+            { 'graph': 'loss', 'titre': 'perte' },
+            { 'graph': 'Weight', 'titre': 'Poids' }
         ]
 
         // EN
         this.titresEN = [
-            {'graph' : 'reserveMiel', 'titre' : 'Honey Stock'},
-            {'graph' : 'DailyWeightIncomes', 'titre' : 'Daily weight incomes'},
-            {'graph' : 'BroodDynamics', 'titre' : 'Brood Dynamics'},
-            {'graph' : 'InternalRelativeHumidity', 'titre' : 'Internal Relative Humidity (max)'},
-            {'graph' : 'InternalTemperature', 'titre' : 'Internal Temperature'},
-            {'graph' : 'ExternalTemperature', 'titre' : 'External Temperature'},
-            {'graph' : 'WeightTemperature', 'titre' : 'Weight & Temperature'},
-            {'graph' : 'Humidity', 'titre' : 'Humidity (%)'},
-            {'graph' : 'loss', 'titre' : 'loss'},
-            {'graph' : 'Weight', 'titre' : 'Weight'}
+            { 'graph': 'reserveMiel', 'titre': 'Honey Stock' },
+            { 'graph': 'DailyWeightIncomes', 'titre': 'Daily weight incomes' },
+            { 'graph': 'BroodDynamics', 'titre': 'Brood Dynamics' },
+            { 'graph': 'InternalRelativeHumidity', 'titre': 'Internal Relative Humidity (max)' },
+            { 'graph': 'InternalTemperature', 'titre': 'Internal Temperature' },
+            { 'graph': 'ExternalTemperature', 'titre': 'External Temperature' },
+            { 'graph': 'WeightTemperature', 'titre': 'Weight & Temperature' },
+            { 'graph': 'Humidity', 'titre': 'Humidity (%)' },
+            { 'graph': 'loss', 'titre': 'loss' },
+            { 'graph': 'Weight', 'titre': 'Weight' }
         ]
 
     }
 
+
+    setImperial() {
+        // If he is French
+        if (this.userService.getJwtReponse().country === "FR") {
+            this.weight.name = 'Poids lbs';
+            this.humidity.name = 'HUmidité %';
+            // EN
+        } else {
+            this.weight.name = 'Weight lbs';
+            this.humidity.name = 'Humidity %'
+        }
+        this.humidity.min = 0;
+        this.humidity.max = 100;
+        this.weight.min = 40;
+        this.weight.max = null;
+        this.humidity.unitT = '%';
+        this.weight.unitW = 'lbs';
+        this.weight.interval = 5;
+        // If he is French
+        if (this.userService.getJwtReponse().country === "FR") {
+            this.temp.name = 'Température (°F)';
+            // EN
+        } else {
+            this.temp.name = 'Temperature (°F)';
+        }
+        this.temp.min = 0;
+        this.temp.unitT = '° F';
+        this.temp.max = null;
+        console.log(this.weight);
+    }
+
+    setMetric() {
+        // If he is French
+        if (this.userService.getJwtReponse().country === "FR") {
+            this.weight.name = 'Poids Kg';
+            this.humidity.name = 'HUmidité %';
+
+            // EN
+        } else {
+            this.weight.name = 'Weight Kg';
+            this.humidity.name = 'Humidity %'
+
+        }
+        this.humidity.name = 'Humidity %'
+        this.humidity.min = 0;
+        this.humidity.max = 100;
+        this.weight.min = 0;
+        this.humidity.unitT = '%';
+        this.weight.unitW = 'Kg';
+        this.weight.interval = 10;
+        this.weight.max = null;
+        // If he is French
+        if (this.userService.getJwtReponse().country === "FR") {
+            this.temp.name = 'Température (°C)';
+            // EN
+        } else {
+            this.temp.name = 'Temperature (°C)';
+        }
+        this.temp.unitT = '° C';
+        this.temp.min = 0;
+        this.temp.max = null;
+        console.log(this.weight);
+    }
     getWeight(): Object {
         return this.weight;
     }
@@ -151,39 +160,39 @@ export class GraphGlobal {
     }
 
     // Here there are all the graph titles
-    getTitle( nomGraphe : String) : any {
-        var titre : any;
+    getTitle(nomGraphe: String): any {
+        var titre: any;
 
         //Found the title with the name of the graph with the right language
 
         // If he is French
-        if(this.userService.getJwtReponse().country === "FR"){
+        if (this.userService.getJwtReponse().country === "FR") {
             titre = this.titresFR[this.titresFR.map(elt => elt.graph).indexOf(nomGraphe)];
-        // EN
-        }else{
+            // EN
+        } else {
             titre = this.titresEN[this.titresEN.map(elt => elt.graph).indexOf(nomGraphe)];
         }
 
         return titre.titre;
     }
 
-    getDays() : String[] {
+    getDays(): String[] {
         // If he is French
-        if(this.userService.getJwtReponse().country === "FR"){
-            return(['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa']);
-        // EN
-        }else{
-            return(['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']);
+        if (this.userService.getJwtReponse().country === "FR") {
+            return (['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa']);
+            // EN
+        } else {
+            return (['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']);
         }
     }
 
-    getMonth() : String[] {
+    getMonth(): String[] {
         // If he is French
-        if(this.userService.getJwtReponse().country === "FR"){
-            return(['Jan','Fev','Mar','Avr','Mai','Juin','Jui','Aou','Sep','Oct','Nov','Dec']);
-        // EN
-        }else{
-            return(['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']);
+        if (this.userService.getJwtReponse().country === "FR") {
+            return (['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Jui', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec']);
+            // EN
+        } else {
+            return (['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']);
         }
     }
 
--- src/app/dashboard/melli-charts/melli-charts.component.ts
+++ src/app/dashboard/melli-charts/melli-charts.component.ts
@@ -1,14 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import { CalendarTemplateService } from './service/calendar-template.service';
-import { ECharts } from 'echarts';
-import { RucherService } from '../service/rucher.service';
-import { UserloggedService } from '../../userlogged.service';
-import { RucheInterface } from '../../_model/ruche';
-import { DailyRecordsWService } from '../apiary/ruche-rucher/ruche-detail/service/daily-records-w.service';
-import { MelliChartsService } from './service/melli-charts.service';
-import { DailyRecordService } from '../service/dailyRecordService';
-import { Observable } from 'rxjs';
-import 'rxjs/add/observable/forkJoin';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-melli-charts',
@@ -17,131 +7,9 @@ import 'rxjs/add/observable/forkJoin';
 })
 export class MelliChartsComponent implements OnInit {
 
-  public echartInstance: ECharts;
-  private currentHiveItem: EventTarget;
-  private currentTypeElt: EventTarget;
-  private hiveSelect: RucheInterface;
-  private typeStrChart: string;
-  public loading: boolean;
-
-  constructor(
-    public templateCalendar: CalendarTemplateService,
-    public rucherService: RucherService,
-    private userService: UserloggedService,
-    public melliService: MelliChartsService,
-    public dailyReccordThService: DailyRecordService,
-    public dailyRecordWService: DailyRecordsWService,
-    private renderer: Renderer2) {
-    this.loading = false;
-    this.currentHiveItem = null;
-    this.currentTypeElt = null;
-    this.hiveSelect = {
-      id: null,
-      name: '',
-      description: '',
-      username: '',
-      idApiary: '',
-      apiaryName: '',
-      idUsername: '',
-      hivePosX: '',
-      hivePosY: '',
-      sharingUser: []
-    };
-    this.typeStrChart = null;
-    this.rucherService.rucheService.getHiveByUsername(this.userService.getUser()).subscribe(
-      data => {
-        this.rucherService.rucheService.ruchesAllApiary = data;
-      }
-    );
-  }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  public onChartInit(event: ECharts) {
-    this.echartInstance = event;
-  }
-
-  public onSelectHive(event: MouseEvent, hive: RucheInterface) {
-    if (this.currentHiveItem !== null && event.target !== this.currentHiveItem) {
-      this.renderer.removeClass(this.currentHiveItem, 'hive-active');
-      this.currentHiveItem = event.target;
-      this.renderer.addClass(this.currentHiveItem, 'hive-active');
-    } else if (this.currentHiveItem === null) {
-      this.currentHiveItem = event.target;
-      this.renderer.addClass(this.currentHiveItem, 'hive-active');
-    }
-    if (this.typeStrChart != null) { // Si un type à été selectionné
-      if (this.hiveSelect.id !== hive.id) { // si la ruche est differente de la précedente
-        this.hiveSelect = hive;
-        this.setData();
-      }
-    } else {
-      this.hiveSelect = hive;
-    }
-  }
-  public selectType(event: MouseEvent, type: string) {
-    this.typeStrChart = type;
-    /* Si je n'ai pas de type et qu'il est different du précédent */
-    if (this.currentTypeElt !== null && this.currentTypeElt !== event.target) {
-      this.renderer.removeClass(this.currentTypeElt, 'type-active');
-      this.currentTypeElt = event.target;
-      this.renderer.addClass(this.currentTypeElt, 'type-active');
-    } else if (this.currentTypeElt === null) {
-      this.currentTypeElt = event.target;
-      this.renderer.addClass(this.currentTypeElt, 'type-active');
-    }
-    /* Si une ruche est séléctionné ET que aucune donnée n'existe la concernant */
-    if (this.currentHiveItem !== null && this.melliService.getMergeAllData() !== null) {
-      this.melliService.setMerge(this.melliService.getMergeAllData()[this.typeStrChart]);
-    } else if (this.currentHiveItem !== null) {
-      this.setData();
-    }
-    this.refreshDataGraph();
-  }
-
-  refreshDataGraph(): void {
-    const options = this.echartInstance.getOption();
-    options.series = this.melliService.getMerge().series;
-    options.tooltip = this.melliService.getMerge().tooltip;
-    options.legend = this.melliService.getMerge().legend;
-    options.visualMap = this.melliService.getMerge().visualMap;
-    this.echartInstance.clear();
-    this.echartInstance.setOption(options);
-  }
-
-
-  setRangeCalendar(): void {
-    const options = this.echartInstance.getOption();
-    options.calendar[0].range = [this.melliService.startCalendar, this.melliService.endCalendar];
-    this.echartInstance.clear();
-    this.echartInstance.setOption(options);
-  }
-  setData(): void {
-    this.loading = true;
-/*     const obsData = [
-      this.dailyRecordWService.getDailyRecordWByHive(this.hiveSelect.id, this.hiveSelect.name),
-      this.dailyReccordThService.getByHive(this.hiveSelect.id)
-    ];
-    console.log(obsData);
-    Observable.forkJoin(obsData).flatMap(res => {
-      console.log(res);
-      return res;
-    }).subscribe(
-      data => {
-        console.log(data);
-      }
-    ); */
-    this.dailyRecordWService.getDailyRecordWByHive(this.hiveSelect.id, this.hiveSelect.name).subscribe(
-      data => {
-        this.melliService.setMergeAllData(data);
-        this.melliService.setMerge(data[this.typeStrChart]);
-        this.refreshDataGraph();
-      },
-      err => { },
-      () => {
-        this.loading = false;
-      }
-    );
-  }
 }
