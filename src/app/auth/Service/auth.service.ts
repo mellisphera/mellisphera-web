e4cb1e3a8a50560370a76d5c91064c269caab65c
mickael2407 <pezzoni.mickael@gmail.com>
2020-02-20 15:14:34 +0100

    fix issue #126

--- src/app/auth/Service/auth.service.ts
+++ src/app/auth/Service/auth.service.ts
@@ -88,13 +88,7 @@ export class AuthService {
         if (this.jwtReponse.connexions === 1) {
           this.userService.setFristConnection(true);
         }
-        if (this.tokenService.checkAuthorities('ROLE_ADMIN')) {
-            this.router.navigateByUrl('dashboard/admin');
-
-        } else {
-          this.router.navigateByUrl('dashboard/home/info-apiary');
-
-        }
+        this.router.navigateByUrl('dashboard/home/info-apiary');
       },
       (err) => {
         this.errLogin = true;
